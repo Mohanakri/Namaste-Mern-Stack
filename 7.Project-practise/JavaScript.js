9925420578bf1204c5004d705c15ce16c7173ab7
Mohanakri <mohanakrishna109@gmail.com>
2025-10-18 17:56:35 +0530

    update

--- 7.Project-practise/JavaScript.js
+++ 7.Project-practise/JavaScript.js
@@ -1,2 +1,20 @@
-const [val = 10] = [];
-console.dir([val]);
+
+
+
+getWeather(city) {
+  const ab = new Promise((resolve,reject)=>{
+    if(city=='Mumbai'){
+      resolve('Weather in Mumbai is sunny');
+    }
+    else
+      reject('City Paris not found');
+  })
+}
+
+ab 
+ .then(result => {
+  console.log('23c)
+ })
+ .then(result1 => {
+  retconsole.log('Enjoy your day!))
+ .catch(result2 => console.log('error bro'));
